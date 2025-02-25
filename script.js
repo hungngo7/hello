d28d0cfb1909419961a253996bcf1194d0f54227
hungngo7 <hung0711209@gmail.com>
2025-02-25 07:23:47 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="xuanhung" && p="123")
+  {
+   window.open()
+   document.geêtlementById(compiler).style.display="block"
+  }
+}
